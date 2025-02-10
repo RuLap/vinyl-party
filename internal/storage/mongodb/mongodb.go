b21b1458b2f79098af89e76469093a6a13480bf5
Igor Meshkov <48246916+RuLap@users.noreply.github.com>
2025-02-11 02:12:12 +0500

    Added mongodb client

--- internal/storage/mongodb/mongodb.go
+++ internal/storage/mongodb/mongodb.go
@@ -1,1 +1,31 @@
 package mongodb
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Storage struct {
+	db *mongo.Client
+}
+
+var client *mongo.Client
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.mongodb.New"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(storagePath))
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: client}, nil
+}
