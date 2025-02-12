7e4b0bcc7757458c26736d61a7f3ead948aae659
Igor Meshkov <meshkovia@susu.ru>
2025-02-12 15:48:06 +0500

    Domain fix

--- internal/dto/rating_dto.go
+++ internal/dto/rating_dto.go
@@ -1,14 +1,12 @@
 package dto
 
 type RatingCreateDTO struct {
-	User  UserShortInfoDTO `json:"user"`
-	Album AlbumInfoDTO     `json:"album"`
-	Score int              `json:"score"`
+	UserID string `json:"user_id"`
+	Score  int    `json:"score"`
 }
 
 type RatingInfoDTO struct {
-	ID   string           `json:"id"`
-	User UserShortInfoDTO `json:"user"`
-
-	Score int `json:"score"`
+	ID    string           `json:"id"`
+	User  UserShortInfoDTO `json:"user"`
+	Score int              `json:"score"`
 }
--- internal/entity/rating.go
+++ internal/entity/rating.go
@@ -1,8 +1,7 @@
 package entity
 
 type Rating struct {
-	ID      string `bson:"_id"`
-	UserID  string `bson:"user_id"`
-	AlbumID string `bson:"album_id"`
-	Score   int    `bson:"score"`
+	ID     string `bson:"_id"`
+	UserID string `bson:"user_id"`
+	Score  int    `bson:"score"`
 }
